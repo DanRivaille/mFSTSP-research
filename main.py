a504ed28e0c7f7066ecb2947975337ecce4118cd
DanRivaille <dan.santos.hd@outlook.com>
2021-09-19 21:01:43 -0300

    Se dibuja la ruta del tsp

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,6 +9,23 @@ def get_deliveriable_points_by_drones(data):
 def get_not_deliveriable_points_by_drones(data):
     filtered_data = df[(df['nodeType'] != 0) & ((df['altMeters'] != 0) | (df['parcelWtLbs'] > 5))]
     return filtered_data
+
+def get_tsp_points(data):
+    quants_points_deliveriable = data.shape[0] - 1
+    tsp_route = random.sample(range(1, quants_points_deliveriable + 1), quants_points_deliveriable)
+    tsp_route = [0] + tsp_route + [0]
+    return tsp_route
+
+def get_tsp_coords(data, tsp_route_points):
+    lat_tsp = []
+    lon_tsp = []
+
+    for point in tsp_route_points:
+        lat_tsp.append(data.iloc[point]['latDeg'])
+        lon_tsp.append(data.iloc[point]['lonDeg'])
+
+    return lat_tsp, lon_tsp
+
 
 # Settings of pyplot
 plt.rcParams["figure.figsize"] = (20, 10)
@@ -24,8 +42,8 @@ max_lon = df['lonDeg'].max()
 min_lon = df['lonDeg'].min()
 
 delta = 0.01
-plt.ylim(min_lon + delta, max_lon + delta)
-plt.xlim(min_lat + delta, max_lat + delta)
+plt.ylim(min_lon - delta, max_lon + delta)
+plt.xlim(min_lat - delta, max_lat + delta)
 
 
 
@@ -40,6 +58,13 @@ plt.scatter(deliveriable_points_by_drones['latDeg'], deliveriable_points_by_dron
 
 # Plot the not deliveriable point by drones (only trucks)
 plt.scatter(not_deliveriable_points_by_drones['latDeg'], not_deliveriable_points_by_drones['lonDeg'], marker='s', color='red')
+
+# TSP Route
+tsp_route_points = get_tsp_points(df)
+print(tsp_route_points)
+
+x, y = get_tsp_coords(df, tsp_route_points)
+plt.plot(x, y, linewidth=1)
 
 plt.show()
 
