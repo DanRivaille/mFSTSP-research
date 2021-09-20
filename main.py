5c945aeb21d61b72931633d9bf013f659f2c90bd
DanRivaille <dan.santos.hd@outlook.com>
2021-09-19 21:26:44 -0300

    Se agrega una flecha indicando hacia donde sale el camion

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def get_not_deliveriable_points_by_drones(data):
 def get_tsp_points(data):
     quants_points_deliveriable = data.shape[0] - 1
     tsp_route = random.sample(range(1, quants_points_deliveriable + 1), quants_points_deliveriable)
+    #tsp_route = list(range(1, quants_points_deliveriable + 1))
     tsp_route = [0] + tsp_route + [0]
     return tsp_route
 
@@ -26,45 +27,59 @@ def get_tsp_coords(data, tsp_route_points):
 
     return lat_tsp, lon_tsp
 
+def draw_tsp_route(data, tsp_route_points):
+    x, y = get_tsp_coords(data, tsp_route_points)
+    plt.plot(x, y, linewidth=1)
+    plt.arrow(x[0], y[0], (x[1] - x[0]) / 2, (y[1] - y[0]) / 2, width=0.0005)
+
+
+def set_plot_limits(data, delta=0.01):
+    max_lat = data['latDeg'].max()
+    min_lat = data['latDeg'].min()
+
+    max_lon = data['lonDeg'].max()
+    min_lon = data['lonDeg'].min()
+
+    plt.ylim(min_lon - delta, max_lon + delta)
+    plt.xlim(min_lat - delta, max_lat + delta)
+
+
+def load_data(filename):
+    names = ['nodeType', 'latDeg', 'lonDeg', 'altMeters', 'parcelWtLbs']
+    data = pd.read_csv(filename, names=names, skiprows=1, index_col=0)
+    return data
+
+def draw_points(data):
+    deliveriable_points_by_drones = get_deliveriable_points_by_drones(data)
+    not_deliveriable_points_by_drones = get_not_deliveriable_points_by_drones(data)
+
+    # Plot the deposit
+    plt.scatter(data.iloc[0]['latDeg'], data.iloc[0]['lonDeg'], marker='^', s=150, color='black')
+
+    # Plot the deliveriable point by drones
+    plt.scatter(deliveriable_points_by_drones['latDeg'], deliveriable_points_by_drones['lonDeg'], color='green')
+
+    # Plot the not deliveriable point by drones (only trucks)
+    plt.scatter(not_deliveriable_points_by_drones['latDeg'], not_deliveriable_points_by_drones['lonDeg'], marker='s', color='red')
+
 
 # Settings of pyplot
 plt.rcParams["figure.figsize"] = (20, 10)
 
 # Load data
-names = ['nodeType', 'latDeg', 'lonDeg', 'altMeters', 'parcelWtLbs']
-df = pd.read_csv('problems/example.csv', names=names, skiprows=1, index_col=0)
+df = load_data('problems/example.csv')
 
-# Plot things
-max_lat = df['latDeg'].max()
-min_lat = df['latDeg'].min()
+# Plot limits
+set_plot_limits(df)
 
-max_lon = df['lonDeg'].max()
-min_lon = df['lonDeg'].min()
-
-delta = 0.01
-plt.ylim(min_lon - delta, max_lon + delta)
-plt.xlim(min_lat - delta, max_lat + delta)
-
-
-
-deliveriable_points_by_drones = get_deliveriable_points_by_drones(df)
-not_deliveriable_points_by_drones = get_not_deliveriable_points_by_drones(df)
-
-# Plot the deposit
-plt.scatter(df.iloc[0]['latDeg'], df.iloc[0]['lonDeg'], marker='^', s=150, color='black')
-
-# Plot the deliveriable point by drones
-plt.scatter(deliveriable_points_by_drones['latDeg'], deliveriable_points_by_drones['lonDeg'], color='green')
-
-# Plot the not deliveriable point by drones (only trucks)
-plt.scatter(not_deliveriable_points_by_drones['latDeg'], not_deliveriable_points_by_drones['lonDeg'], marker='s', color='red')
+# Plot 
+draw_points(df)
 
 # TSP Route
 tsp_route_points = get_tsp_points(df)
 print(tsp_route_points)
 
-x, y = get_tsp_coords(df, tsp_route_points)
-plt.plot(x, y, linewidth=1)
+draw_tsp_route(df, tsp_route_points)
 
 plt.show()
 
